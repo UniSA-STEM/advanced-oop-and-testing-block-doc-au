aa6e240ba927a0dd4fb3054b21dfe485fea83784
Neil Cochrane <neilc@cns-gc.com.au>
2025-11-04 21:37:57 +1000

    Initial commit after user details and id entered on py files

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+File: main.py
+Description: A brief description of this Python module.
+Author: Neil John Cochrane
+ID: 110484750
+Username: COCNJ001
+This is my own work as defined by the University's Academic Misconduct Policy.
+"""
+
